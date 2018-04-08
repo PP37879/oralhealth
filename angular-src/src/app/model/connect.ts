4dc786ba3a1d4bb8fe40aacfac2b51e28e3be38f
PP37879 <pp37879@gmail.com>
2018-04-08 12:28:42 +0700

    Fix url for register

--- angular-src/src/app/model/connect.ts
+++ angular-src/src/app/model/connect.ts
@@ -1,5 +1,5 @@
 export class Connect {
-    private static hostUrl = 'oralhealthstatuscheck.com';
+    private static hostUrl = 'http://oralhealthstatuscheck.com';
     public static readonly USER_TOKEN = 'user_token';
     static getHostUrl():string {
       return this.hostUrl;
